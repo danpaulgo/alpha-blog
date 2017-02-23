eb120345f39e9f72dbdeffc89ad9161ad0503520
Daniel Goldberg <danpaulgo@aol.com>
2017-02-23 04:08:00 -0500

    Added index and edit pages. Added navigation links to all pages

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,5 @@ class Article < ApplicationRecord
 
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :description, presence: true, length: {minimum: 5, maximum: 250}
-  validates :description, presence: true
 
 end
